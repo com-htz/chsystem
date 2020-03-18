6176ad8709e4b0844fae6f115870f29333859cfc
文道长老 <energyfiled@outlook.com>
2020-03-18 22:21:51 +0800

    添加通用类

---
+++ chsystem/chsystem-commons/src/main/java/com/htz/chsystem/commons/dto/PageInfo.java
@@ -0,0 +1,55 @@
+package com.htz.chsystem.commons.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 分页数据传输对象
+ */
+public class PageInfo<T> implements Serializable {
+    private int draw;
+    private int recordsTotal;
+    private int recordsFiltered;
+    private List<T> data;
+    private String error;
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int deaw) {
+        this.draw = deaw;
+    }
+
+    public int getRecordsTotal() {
+        return recordsTotal;
+    }
+
+    public void setRecordsTotal(int recordsTotal) {
+        this.recordsTotal = recordsTotal;
+    }
+
+    public int getRecordsFiltered() {
+        return recordsFiltered;
+    }
+
+    public void setRecordsFiltered(int recordsFiltered) {
+        this.recordsFiltered = recordsFiltered;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
