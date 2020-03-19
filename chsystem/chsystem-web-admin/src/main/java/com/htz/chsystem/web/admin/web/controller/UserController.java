8cd4eb09ebd3b0e13b9ae8d96db5a7d002cf46e6
文道长老 <energyfiled@outlook.com>
2020-03-19 20:54:49 +0800

    用户控制器

---
+++ chsystem/chsystem-web-admin/src/main/java/com/htz/chsystem/web/admin/web/controller/UserController.java
@@ -0,0 +1,133 @@
+package com.htz.chsystem.web.admin.web.controller;
+
+import com.htz.chsystem.commons.dto.BaseResult;
+import com.htz.chsystem.commons.dto.PageInfo;
+import com.htz.chsystem.domain.TbUser;
+import com.htz.chsystem.web.admin.service.TbUserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping(value = "user")
+public class UserController {
+
+    @Autowired
+    private TbUserService tbUserService;
+
+    @ModelAttribute
+    public TbUser getTbUser(Long id) {
+        TbUser tbUser = null;
+
+        // id 不为空，则从数据库获取
+        if (id != null) {
+            tbUser = tbUserService.getById(id);
+        } else {
+            tbUser = new TbUser();
+        }
+
+        return tbUser;
+    }
+
+    /**
+     * 跳转到用户列表页
+     *
+     * @return
+     */
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public String list() {
+        return "user_list";
+    }
+
+    /**
+     * 跳转用户表单页
+     *
+     * @return
+     */
+
+    @RequestMapping(value = "form", method = RequestMethod.GET)
+    public String form() {
+        return "user_form";
+    }
+
+    /**
+     * 保存用户信息
+     *
+     * @param tbUser
+     * @return
+     */
+
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String save(TbUser tbUser, Model model, RedirectAttributes redirectAttributes) {
+        BaseResult baseResult = tbUserService.save(tbUser);
+
+        // 保存成功
+        if (baseResult.getStatus() == 200) {
+            redirectAttributes.addFlashAttribute("baseResult", baseResult);
+            return "redirect:/user/list";
+        }
+
+        // 保存失败
+        else {
+            model.addAttribute("baseResult", baseResult);
+            return "user_form";
+        }
+    }
+
+    /**
+     * 删除用户信息
+     *
+     * @param ids
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    public BaseResult delete(String ids) {
+        BaseResult baseResult = null;
+        if (StringUtils.isNotBlank(ids)) {
+            String[] idArray = ids.split(",");
+            tbUserService.deleteMulti(idArray);
+            baseResult = BaseResult.success("删除用户成功");
+        } else {
+            baseResult = BaseResult.fail("删除用户失败");
+        }
+
+        return baseResult;
+    }
+
+    /**
+     * 显示用户详情
+     *
+     * @return
+     */
+    @RequestMapping(value = "detail", method = RequestMethod.GET)
+    public String detail() {
+        return "user_detail";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "page", method = RequestMethod.GET)
+    public PageInfo<TbUser> page(HttpServletRequest request, TbUser tbUser) {
+        String strDraw = request.getParameter("draw");
+        String strStart = request.getParameter("start");
+        String strLength = request.getParameter("length");
+
+        int draw = strDraw == null ? 0 : Integer.parseInt(strDraw);
+        int start = strStart == null ? 0 : Integer.parseInt(strStart);
+        int length = strLength == null ? 10 : Integer.parseInt(strLength);
+
+        // 封装 Datatables 需要的结果
+        PageInfo<TbUser> pageInfo = tbUserService.page(start, length, draw, tbUser);
+
+        return pageInfo;
+    }
+}
