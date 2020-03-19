81d87216ff4b6bdcafe65fd037683f9e3637d5c0
文道长老 <energyfiled@outlook.com>
2020-03-19 21:35:35 +0800

    转跳拦截

---
+++ chsystem/chsystem-web-admin/src/main/java/com/htz/chsystem/web/admin/web/interceptor/PermissionInterceptor.java
@@ -0,0 +1,30 @@
+package com.htz.chsystem.web.admin.web.interceptor;
+
+import com.htz.chsystem.commons.constant.ConstantUtils;
+import com.htz.chsystem.domain.TbUser;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PermissionInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        //以 login 结尾的请求
+        if (modelAndView != null && modelAndView.getViewName() != null && modelAndView.getViewName().endsWith("login")){
+            TbUser user = (TbUser) httpServletRequest.getSession().getAttribute(ConstantUtils.SESSION_USER);
+            if (user != null){
+                httpServletResponse.sendRedirect("/main");
+            }
+        }
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
