50eae3e5c8300a4564fcf5bd80c570e00512f7c0
文道长老 <energyfiled@outlook.com>
2020-03-22 11:02:32 +0800

    添加注释

--- chsystem/chsystem-web-admin/src/main/java/com/htz/chsystem/web/admin/web/interceptor/LoginInterceptor.java
+++ chsystem/chsystem-web-admin/src/main/java/com/htz/chsystem/web/admin/web/interceptor/LoginInterceptor.java
@@ -1,23 +1,18 @@
 package com.htz.chsystem.web.admin.web.interceptor;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.htz.chsystem.commons.constant.ConstantUtils;
 import com.htz.chsystem.domain.TbUser;
-import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.Pattern;
-import java.util.Date;
 
 
 /**
  * 登录拦截器
  */
-public class LoginIntercepter implements HandlerInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
 
         TbUser user = (TbUser) httpServletRequest.getSession().getAttribute(ConstantUtils.SESSION_USER);
@@ -31,11 +26,11 @@ public class LoginIntercepter implements HandlerInterceptor {
         return true;
     }
 
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) {
+        // empty
     }
 
-    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e){
+        // empty
     }
 }
