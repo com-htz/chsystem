14179046cad302e48131079cfbe98b3647b7897b
文道长老 <energyfiled@outlook.com>
2020-03-22 11:06:10 +0800

    添加注释

---
+++ chsystem/chsystem-commons/src/main/java/com/htz/chsystem/commons/constant/ConstantUtils.java
@@ -0,0 +1,14 @@
+package com.htz.chsystem.commons.constant;
+
+/**
+ * 常用关键字
+ * <p>Title: ConstantUtils</p>
+ * <p>Description: </p>
+ *
+ * @author EnergyFiled
+ * @version 1.0.0
+ */
+
+public class ConstantUtils {
+    public static final String SESSION_USER = "user";
+}
