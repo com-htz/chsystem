a5f7e752eb9e9d439c3f1e36a3151acd6ba787d7
文道长老 <energyfiled@outlook.com>
2020-03-30 21:54:47 +0800

    删除无用导入

--- chsystem/chsystem-web-admin/src/main/java/com/htz/chsystem/web/admin/dao/TbWorkerDao.java
+++ chsystem/chsystem-web-admin/src/main/java/com/htz/chsystem/web/admin/dao/TbWorkerDao.java
@@ -3,9 +3,6 @@ package com.htz.chsystem.web.admin.dao;
 import com.htz.chsystem.domain.TbWorker;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Map;
-
 /**
  * 用户数据访问层
  * <p>Title: TbWorkerDao</p>
@@ -16,63 +13,12 @@ import java.util.Map;
  */
 
 @Repository
-public interface TbWorkerDao {
-
-    /**
-     * 查询全部信息
-     * @return
-     */
-    List<TbWorker> selectAll();
-
-    /**
-     * 新增
-     * @param entity
-     */
-    void insert(TbWorker entity);
-
-    /**
-     * 删除
-     * @param id
-     */
-    void delete(Long id);
-
-    /**
-     * 更新信息
-     * @param entity
-     */
-    void update(TbWorker entity);
-
-    /**
-     * 根据 ID 查询信息
-     * @param id
-     * @return
-     */
-    TbWorker getById(Long id);
-
+public interface TbWorkerDao extends BaseDao<TbWorker> {
     /**
      * 根据手机号查询用户信息
      * @param phone
      * @return
      */
     TbWorker getByPhone(String phone);
-
-    /**
-     * 批量删除
-     * @param ids
-     */
-    void deleteMulti(String[] ids);
-
-    /**
-     * 分页查询
-     * @param params 两个参数， start/记录开始位置, length/每页记录数
-     * @return
-     */
-    List<TbWorker> page(Map<String, Object> params);
-
-    /**
-     * 查询总结果数
-     * @return
-     */
-    int count(TbWorker entity);
 
 }
