dc3bc75326b7449ade56abd6c6b84ae0f6f20b48
文道长老 <energyfiled@outlook.com>
2020-03-30 21:52:56 +0800

    班级学生Dao

---
+++ chsystem/chsystem-web-admin/src/main/java/com/htz/chsystem/web/admin/dao/TbTeamStudentDao.java
@@ -0,0 +1,8 @@
+package com.htz.chsystem.web.admin.dao;
+
+import com.htz.chsystem.domain.TbTeamStudent;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TbTeamStudentDao extends BaseDao<TbTeamStudent> {
+}
